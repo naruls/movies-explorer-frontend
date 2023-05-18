5abe003a32c3e2f1a08fbe609b40b367f7b8b39f
naruls <76217299+naruls@users.noreply.github.com>
2023-05-18 17:26:50 +0300

    Добавлено закрытие по нажатию оверлея

--- src/components/ProfileChangePopup/ProfileChangePopup.js
+++ src/components/ProfileChangePopup/ProfileChangePopup.js
@@ -1,7 +1,7 @@
 function ProfileChangePopup(props) {
     return(
             <div className={props.isChangeProfilePopupOpen ? "profileChangePopup" : "profileChangePopup profileChangePopup_hidden"}>
-                <div className="profileChangePopup__overlay"></div>
+                <div className="profileChangePopup__overlay" onClick={props.closeChangeProfilePopup}></div>
                 <div className="profileChangePopup__content">
                     <p className="profileChangePopup__title">Данные профиля успешно обновленны</p>
                     <button className="profileChangePopup__button" onClick={props.closeChangeProfilePopup}>Ок</button>
